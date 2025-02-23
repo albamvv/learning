e35a21e6f684c1320214bc535e36db67233b2f6c
Alba Maitia Vadillo Valderrama <amvadillo@isdefe.es>
2025-02-23 17:11:30 +0100

    prompt

--- Base_LLM_vs_Fine_Tuned_LLM.py
+++ Base_LLM_vs_Fine_Tuned_LLM.py
@@ -1,4 +1,7 @@
 from transformers import T5Tokenizer, T5ForConditionalGeneration
+import torch
+from prompt import prompt1, prompt2,prompt3,prompt4,prompt5
+
 
 '''
 T5Tokenizer: Para convertir texto en tokens que el modelo puede entender.
@@ -15,3 +18,23 @@ device_map="auto" permite asignar automáticamente el modelo a GPU o CPU, depend
 '''
 # Importamos el modelo pre-entrenado
 model_T5 = T5ForConditionalGeneration.from_pretrained("t5-base", device_map="auto")
+
+# Este código funcionará en CPU sin problemas. Si en el futuro activas CUDA, también lo usará automáticamente.
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+#print(f"Usando: {device}")
+
+
+# Tokenizamos el prompt
+#prompt_tokens = tokenizer_T5(prompt, return_tensors="pt").input_ids.to("cuda")
+prompt_tokens = tokenizer_T5(prompt, return_tensors="pt").input_ids.to(device)
+
+# Generamos los siguientes tokens
+outputs = model_T5.generate(prompt_tokens, max_length=100) # Coge el prompt que se le ha pasado y continua haciendo cosas pero no hace funciones
+
+# Transformamos los tokens generados en texto
+'''
+Presenta el comportamiento de un LLM base, no han sido entrenados para ninguna tarea especifica, ni para conversacion, ni para responder preguntas. 
+Sólo presentan el comportamiento primitivo de un LLM, que es generar la siguiente palabra, a partir del prompt que se le indica genera la siguiente palabra,
+y continua generando palabras que en este caso coinciden en función del contexto con lo que se habia puesto anteriormente
+'''
+print(tokenizer_T5.decode(outputs[0]))
---
+++ prompt.py
@@ -0,0 +1,51 @@
+#### PROMPT BÁSICOS
+prompt1 = "My name"
+prompt2 = "Today is"
+prompt3 = "Me llamo"
+
+text = """The Second World War (also written World War II)1 was a global military \
+conflict that took place between 1939 and 1945. It involved most of the world's \
+nations - including all the major powers, as well as virtually all European nations \
+- grouped into two opposing military alliances: the Allies on the one hand, and the \
+Axis Powers on the other. It was the greatest war in history, with more than 100 \
+million military personnel mobilized and a state of total war in which the major \
+contenders devoted all their economic, military and scientific capabilities to the \
+service of the war effort, blurring the distinction between civilian and military \
+resources."""
+
+prompt4 = f"Summarize: {text}"
+
+prompt5 = "What do you think of Mars?"
+
+review1 = """Love these plugs, have a few now. We use them to plug in lights and \
+set timers to turn them on and off via a phone app. Easy to use and linked to \
+the internet and apps. Good value for money."""
+
+review2 = """Tried and tried but could never get them to work right. Too bad \
+I'm past my return date or they would have gone back."""
+
+review3 = """A well-sized, reliable smart plug. The app is easy to use and set \
+up, and works well. I used them to make several lamps. Everything works fine - \
+no problems."""
+
+review4 = """Great little product. Super easy to set up. Didn't even need to use \
+the Alexa app to do so. Did it with my echo. Now I use it almost daily to turn on \
+a light that was a pain to get to."""
+
+review5 = """If I could give this zero stars I would. Plug wouldn’t connect. I \
+had to keep connecting it and finally just gave up and returned it. Customer service \
+was a complete waste of time."""
+
+prompt6 = f"""
+Review: {review1}
+Sentiment: Positive
+
+Review: {review2}
+Sentiment: Negative
+
+Review: {review3}
+Sentiment: Positive
+
+Review: {review5}
+Sentiment:"""
+
